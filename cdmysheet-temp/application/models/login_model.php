131c3209b18fe7c7be6c88251181eb6aa4a039ca
Kamal1412 <rajkamalg@cybytetech.com>
2016-02-15 19:13:08 +0530

    first commit of cdmysheet

---
+++ cdmysheet-temp/application/models/login_model.php
@@ -0,0 +1,41 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class login_model extends CI_Model
+{
+     function __construct()
+     {
+          // Call the Model constructor
+          parent::__construct();
+     }
+
+     //get the username & password from tbl_usrs
+     function get_user($username, $password)
+     {
+          $sql = "select * from tbl_usrs where username = '" . $username . "' and password = '" . $password . "' limit 1";
+		  
+		  
+            $query = $this->db->query($sql);
+          return $query->num_rows();
+		  
+     }
+	 
+	 function get_user_details($username)
+     {
+          $sql = "select * from tbl_usrs,myinformationpersonal  where username = '" . $username . "' and  tbl_usrs.empid = myinformationpersonal.empid limit 1";
+        
+		   $query = $this->db->query("$sql");
+		  
+           $result = $query->result_array();
+		  
+		   
+		   
+		  
+		  return $result[0];
+
+		  
+	}
+	 
+	
+	
+}
+?>
